662188e8880b5113fbc99d182832fe6be64632ca
Adeyera16 <asimolowoelizabeth@gmail.com>
2022-06-22 15:12:05 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let name = "ASIMOLOWO ADEYERA"
+alert("My name is " + name)
+
+let height = "5.7 feet"
+alert("My height is " + height)
+
+let country = "Nigeria"
+alert("I am " + country + "n")
+
+alert(name+" , " + height +" , " + country + "n")
